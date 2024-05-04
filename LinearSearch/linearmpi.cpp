f403fce087bcc93f6f015d7bb1efad94ad5057ac
Ambhikesh <101205995+True-Fox@users.noreply.github.com>
2024-05-05 02:32:30 +0530

    Update linearmpi.cpp

--- LinearSearch/linearmpi.cpp
+++ LinearSearch/linearmpi.cpp
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <mpi/mpi.h>
+#include <mpi.h>
 #include <vector>
 #include <fstream>
+#include <iostream>
 
 int linear_search(int arr[], int key, int size) {
     for (int i = 0; i < size; i++) {
@@ -14,26 +15,25 @@ int linear_search(int arr[], int key, int size) {
 }
 
 int main(int argc, char* argv[]) {
-
-    if(argc != 2){
+    if (argc != 2) {
         std::cout << "Usage: " << argv[0] << " <filename>\n";
         return EXIT_FAILURE;
     }
 
-
-
-    int number_of_processes, rank;
+    int i, index, global_index;
+    int rank, number_of_processes;
     MPI_Status status;
 
-    MPI_Init(&argc, &argv);
+    MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &number_of_processes);
 
     if (rank == 0) {
-        //std::vector<int> arr = { 1, 5, 7, 10, 12, 14, 15, 18, 20, 22, 25, 27, 30, 64, 110, 220 };
+        // The master process
+        // Load the array from file
         std::string filename = argv[1];
         std::ifstream file(filename);
-        if(!file.is_open()){
+        if (!file.is_open()) {
             std::cerr << "Error opening file\n";
             MPI_Finalize();
             return EXIT_FAILURE;
@@ -41,7 +41,7 @@ int main(int argc, char* argv[]) {
 
         std::vector<int> _arr;
         int value;
-        while(file >> value){
+        while (file >> value) {
             _arr.push_back(value);
         }
 
@@ -50,54 +50,73 @@ int main(int argc, char* argv[]) {
 
         file.close();
 
-        int number_of_elements = _arr.size();
-        int key = 14;
-        int chunk_size = number_of_elements / number_of_processes;
+        double start = MPI_Wtime();
+        long long key = 820767;
+        int elements_per_process = _arr.size() / number_of_processes;
+        int remaining_elements = _arr.size() % number_of_processes;
         bool found = false;
 
-        int local_index = linear_search(arr, key, chunk_size);
-        if (local_index != -1) {
-            std::cout << "The key " << key << " is found at index " << local_index << std::endl;
+        // Linear search on master process
+        int start_index = 0;
+        int end_index = elements_per_process + (rank == 0 ? remaining_elements : 0) - 1;
+        index = linear_search(arr, key, end_index - start_index + 1);
+        if (index != -1) {
+            std::cout << "The key " << key << " is found at index " << index << std::endl;
             found = true;
         }
 
-        for (int i = 1; i < number_of_processes; i++) {
-            int start_index = i * chunk_size;
-            int chunk_size_remaining = (i == number_of_processes - 1) ? number_of_elements - start_index : chunk_size;
-            MPI_Send(&key, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Send(&chunk_size_remaining, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Send(&start_index, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Send(&arr[start_index], chunk_size_remaining, MPI_INT, i, 0, MPI_COMM_WORLD);
+        // Pack and send information to other processes
+        int send_count;
+        int send_buffer[3];
+        MPI_Request* requests = new MPI_Request[2 * number_of_processes];
+        for (i = 1; i < number_of_processes; i++) {
+            start_index = i * elements_per_process + (i <= remaining_elements ? i : remaining_elements);
+            end_index = start_index + elements_per_process + (i <= remaining_elements ? 0 : -1);
+            send_buffer[0] = key;
+            send_buffer[1] = start_index;
+            send_buffer[2] = end_index - start_index + 1;
+            MPI_Isend(send_buffer, 3, MPI_INT, i, 0, MPI_COMM_WORLD, &requests[(i - 1) * 2]);
+            MPI_Isend(&arr[start_index], end_index - start_index + 1, MPI_INT, i, 0, MPI_COMM_WORLD, &requests[(i - 1) * 2 + 1]);
         }
 
-        for (int i = 1; i < number_of_processes; i++) {
-            int local_index, start_index;
-            MPI_Recv(&local_index, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-            MPI_Recv(&start_index, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD, &status);
-            if (local_index != -1) {
-                int global_index = local_index + start_index;
-                std::cout << "The key " << key << " is found at index " << global_index << std::endl;
+        // Receive result from other processes and analyze the result
+        MPI_Status recv_status;
+        int recv_buffer[3];
+        for (i = 1; i < number_of_processes; i++) {
+            MPI_Recv(&recv_buffer, 3, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &recv_status);
+            index = recv_buffer[0];
+            start_index = recv_buffer[1];
+            int num_of_elements_received = recv_buffer[2];
+            if (index != -1) {
+                global_index = start_index + index;
+                printf("The key %d is found at index %d\n", key, global_index);
                 found = true;
             }
         }
 
         if (!found) {
-            std::cout << "The key " << key << " is not found" << std::endl;
+            printf("The key %d is not found\n", key);
         }
+        double end = MPI_Wtime();
+        printf("Time taken to search: %.8lfms\n", (end - start) * 1000);
+
+        delete[] requests;
     } else {
-        int key, chunk_size, local_index, start_index;
-        MPI_Recv(&key, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-        MPI_Recv(&chunk_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-        MPI_Recv(&start_index, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        // Other processes: Receive info from master process, do linear search, and send the results back to master process
+        MPI_Status recv_status;
+        int recv_buffer[3];
+        MPI_Recv(&recv_buffer, 3, MPI_INT, 0, 0, MPI_COMM_WORLD, &recv_status);
+        int key = recv_buffer[0];
+        int start_index = recv_buffer[1];
+        int num_of_elements_received = recv_buffer[2];
+        int buffer[num_of_elements_received];
+        MPI_Recv(&buffer, num_of_elements_received, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        index = linear_search(buffer, key, num_of_elements_received);
 
-        int count;
-        MPI_Get_count(&status, MPI_INT, &count);
-        std::vector<int> chunk(count);
-
-        MPI_Recv(chunk.data(), chunk_size, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-        local_index = linear_search(chunk.data(), key, chunk_size);
-        MPI_Send(&local_index, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-        MPI_Send(&start_index, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        int send_buffer[2];
+        send_buffer[0] = index;
+        send_buffer[1] = start_index;
+        MPI_Send(send_buffer, 2, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
