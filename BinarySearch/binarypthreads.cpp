06ac005eb0902d7d5b65bd223108ba123f6c5c8b
roseline <roselinejerry17@gmail.com>
2024-05-05 17:42:13 +0530

    added time measurements

--- BinarySearch/binarypthreads.cpp
+++ BinarySearch/binarypthreads.cpp
@@ -1,71 +1,104 @@
 // CPP Program to perform binary search using pthreads
+
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <pthread.h>
+#include <vector>
+#include <string>
+#include <chrono>
 using namespace std;
 
-// size of array
-#define MAX 16
-
-// maximum number of threads
 #define MAX_THREAD 4
 
-// array to be searched
-int a[] = { 1, 5, 7, 10, 12, 14, 15, 18,
-			20, 22, 25, 27, 30, 64, 110, 220 };
-
-// key that needs to be searched
-int key = 110;
 bool found = false;
 int part = 0;
+int key = 820767; // Key to search
+
+struct ThreadArgs {
+    int start;
+    int end;
+    vector<int>* arr; // Pointer to the array to be searched
+};
 
 void* binary_search(void* arg)
 {
+    ThreadArgs* args = (ThreadArgs*)arg;
 
-	// Each thread checks 1/4 of the array for the key
-	int thread_part = part++;
-	int mid;
+    int thread_part = part++;
+    int low = args->start;
+    int high = args->end;
 
-	int low = thread_part * (MAX / 4);
-	int high = (thread_part + 1) * (MAX / 4);
 
-	// search for the key until low < high
-	// or key is found in any portion of array
-	while (low < high && !found) {
+    vector<int>& arr = *(args->arr); // Reference to the array to be searched
 
-		// normal iterative binary search algorithm
-		mid = (high - low) / 2 + low;
+    while (low <= high && !found) {
+        int mid = low + (high - low) / 2;
 
-		if (a[mid] == key) {
-			found = true;
-			break;
-		}
+        if (arr[mid] == key) {
+            found = true;
+            break;
+        } else if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
 
-		else if (a[mid] > key)
-			high = mid - 1;
-		else
-			low = mid + 1;
-	}
+    return NULL;
 }
 
-// Driver Code
-int main()
+int main(int argc, char* argv[])
 {
-	pthread_t threads[MAX_THREAD];
+    if (argc != 2) {
+        cout << "Usage: " << argv[0] << " <filename>" << endl;
+        return 1;
+    }
 
-	for (int i = 0; i < MAX_THREAD; i++)
-		pthread_create(&threads[i], NULL, binary_search,
-					(void*)NULL);
+    string filename = argv[1];
 
-	for (int i = 0; i < MAX_THREAD; i++)
-		pthread_join(threads[i], NULL);
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cout << "Error: Unable to open file " << filename << endl;
+        return 1;
+    }
 
-	// key found in array
-	if (found)
-		cout << key << " found in array" << endl;
+    vector<int> arr;
+    int value;
+    while (file >> value) {
+        arr.push_back(value);
+    }
+    file.close();
 
-	// key not found in array
-	else
-		cout << key << " not found in array" << endl;
+    pthread_t threads[MAX_THREAD];
+    ThreadArgs args[MAX_THREAD];
+    
+    using namespace std::chrono;
+    auto start_time = high_resolution_clock::now(); // Start time before search
 
-	return 0;
+    int chunk_size = arr.size() / MAX_THREAD;
+    for (int i = 0; i < MAX_THREAD; i++) {
+        args[i].start = i * chunk_size;
+        args[i].end = (i == MAX_THREAD - 1) ? arr.size() - 1 : (i + 1) * chunk_size - 1;
+        args[i].arr = &arr; // Passing a pointer to the array to the thread
+        pthread_create(&threads[i], NULL, binary_search, (void*)&args[i]);
+    }
+
+    for (int i = 0; i < MAX_THREAD; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    if (found)
+        cout << key << " found in array" << endl;
+    else
+        cout << key << " not found in array" << endl;
+	
+	auto end_time = high_resolution_clock::now();    // End time after search
+
+    // Calculate elapsed time in milliseconds
+    duration<double, milli> elapsed_time = end_time - start_time;
+
+    cout << "Search time: " << elapsed_time.count() << " milliseconds" << endl;
+    
+    return 0;
 }
 
